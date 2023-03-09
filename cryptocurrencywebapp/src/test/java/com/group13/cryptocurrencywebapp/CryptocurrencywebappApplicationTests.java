ae5a79f2a273f62ddebc9c43a0fc7700051622c1
Felipe Gebara <feagmoreira@hotmail.com>
2023-03-09 14:18:44 -0700

    new back end springboot project

---
+++ cryptocurrencywebapp/src/test/java/com/group13/cryptocurrencywebapp/CryptocurrencywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group13.cryptocurrencywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
