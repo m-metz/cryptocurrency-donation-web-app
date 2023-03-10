6d504eb0bdcb43c61c5b1131aeae5fa5ea289399
AlexanderKruger <104882141+AlexanderKruger@users.noreply.github.com>
2023-03-10 11:48:32 -0700

    Adding Constructors

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/cryptotransfer/CryptoTransfer.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/cryptotransfer/CryptoTransfer.java
@@ -1,5 +1,6 @@
 package com.group13.cryptocurrencywebapp.cryptotransfer;
 
+import java.time.LocalDateTime;
 import com.group13.cryptocurrencywebapp.transaction.Transaction;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -11,4 +12,23 @@ public class CryptoTransfer extends Transaction{
     private int exchangeReferenceId;
     private String transactionType;
 
+    public CryptoTransfer(int id, String currency, float amount, LocalDateTime time,
+            float final_amount, int exchangeReferenceId, String transactionType) {
+        super(id, currency, amount, time, final_amount);
+        this.exchangeReferenceId = exchangeReferenceId;
+        this.transactionType = transactionType;
+    }
+
+    public CryptoTransfer(String currency, float amount, LocalDateTime time, float final_amount,
+            int exchangeReferenceId, String transactionType) {
+        super(currency, amount, time, final_amount);
+        this.exchangeReferenceId = exchangeReferenceId;
+        this.transactionType = transactionType;
+    }
+
+    
+
+    
+
 }
+
--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/trade/Trade.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/trade/Trade.java
@@ -1,5 +1,6 @@
 package com.group13.cryptocurrencywebapp.trade;
 
+import java.time.LocalDateTime;
 import com.group13.cryptocurrencywebapp.transaction.Transaction;
 
 import jakarta.persistence.Entity;
@@ -15,4 +16,22 @@ public class Trade extends Transaction {
 
     private float convertedAmount;
 
+    public Trade(int id, String currency, float amount, LocalDateTime time, float final_amount,
+            String exchangeReferenceId, String toCurrency, float convertedAmount) {
+        super(id, currency, amount, time, final_amount);
+        this.exchangeReferenceId = exchangeReferenceId;
+        this.toCurrency = toCurrency;
+        this.convertedAmount = convertedAmount;
+    }
+
+    public Trade(String currency, float amount, LocalDateTime time, float final_amount,
+            String exchangeReferenceId, String toCurrency, float convertedAmount) {
+        super(currency, amount, time, final_amount);
+        this.exchangeReferenceId = exchangeReferenceId;
+        this.toCurrency = toCurrency;
+        this.convertedAmount = convertedAmount;
+    }
+
+
+    
 }
