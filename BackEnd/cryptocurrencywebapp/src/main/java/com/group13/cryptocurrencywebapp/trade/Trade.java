9c21287c18f104700567a811201a34dab6abf30a
Felipe Gebara <feagmoreira@hotmail.com>
2023-03-09 18:03:10 -0700

    transcation and trade attributes mapping

--- BackEnd/cryptocurrencywebapp/src/main/java/com/group13/cryptocurrencywebapp/trade/Trade.java
+++ BackEnd/cryptocurrencywebapp/src/main/java/com/group13/cryptocurrencywebapp/trade/Trade.java
@@ -1,5 +1,18 @@
 package com.group13.cryptocurrencywebapp.trade;
 
-public class Trade {
+import com.group13.cryptocurrencywebapp.transaction.Transaction;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tax_receipt")
+public class Trade extends Transaction {
+
+    private String exchangeReferenceId;
+
+    private String toCurrency;
+
+    private float convertedAmount;
 
 }
--- BackEnd/cryptocurrencywebapp/src/main/java/com/group13/cryptocurrencywebapp/transaction/Transaction.java
+++ BackEnd/cryptocurrencywebapp/src/main/java/com/group13/cryptocurrencywebapp/transaction/Transaction.java
@@ -1,5 +1,28 @@
 package com.group13.cryptocurrencywebapp.transaction;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "transaction")
 public class Transaction {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tax_receipt_id")
+    private int id;
+
+    private String currency;
+
+    private float amount;
+
+    private LocalDateTime time;
+
+    private float final_amount;
+
 }
