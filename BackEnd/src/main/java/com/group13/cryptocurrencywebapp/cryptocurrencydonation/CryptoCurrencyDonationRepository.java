2364da2af6d340ff98a49dd03d3570c68e45baac
Felipe Gebara <feagmoreira@hotmail.com>
2023-03-12 23:25:49 -0600

    Table and Mapping corrections

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/cryptocurrencydonation/CryptoCurrencyDonationRepository.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/cryptocurrencydonation/CryptoCurrencyDonationRepository.java
@@ -1,5 +1,9 @@
 package com.group13.cryptocurrencywebapp.cryptocurrencydonation;
 
-public class CryptoCurrencyDonationRepository {
-    
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface CryptoCurrencyDonationRepository extends JpaRepository<CryptoCurrencyDonation, Integer> {
+
 }
