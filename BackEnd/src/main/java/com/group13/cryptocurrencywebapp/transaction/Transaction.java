00c7b33bdec323797a0f9f895a70e9fbc14ddabc
AlexanderKruger <104882141+AlexanderKruger@users.noreply.github.com>
2023-03-13 11:15:49 -0600

    Commenting unused import

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/transaction/Transaction.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/transaction/Transaction.java
@@ -3,7 +3,7 @@ package com.group13.cryptocurrencywebapp.transaction;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.group13.cryptocurrencywebapp.fee.Fee;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
