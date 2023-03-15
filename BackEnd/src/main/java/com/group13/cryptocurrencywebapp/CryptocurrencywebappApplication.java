e0e5f3e9373ca77583b5bf93824c21db4bf8f61b
Felipe Gebara <feagmoreira@hotmail.com>
2023-03-15 15:04:58 -0600

    pushing oauth config

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/CryptocurrencywebappApplication.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/CryptocurrencywebappApplication.java
@@ -2,6 +2,7 @@ package com.group13.cryptocurrencywebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CryptocurrencywebappApplication {
