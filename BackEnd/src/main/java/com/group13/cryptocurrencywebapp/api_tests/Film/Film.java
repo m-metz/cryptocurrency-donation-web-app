a3f27ae574ff730afb26072768fe8de541813b6c
AlexanderKruger <104882141+AlexanderKruger@users.noreply.github.com>
2023-03-15 15:07:50 -0600

    Creating Example Creating Film API caller Adding Rest Template Bean to main

---
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/api_tests/Film/Film.java
@@ -0,0 +1,23 @@
+package com.group13.cryptocurrencywebapp.api_tests.Film;
+
+public class Film {
+    
+    private String title;
+    private String orginal_title;
+    private String original_title_romanized;
+
+    public String getTitle(){
+        return title;
+    }
+    
+    public String getOriginal_title(){
+        return orginal_title;
+    }
+
+    public String getOriginal_title_romanized(){
+        return original_title_romanized;
+    }
+}
+
+
+
