51b9cb750a3ed7edd5fe1266ff38a241bf15b17f
Michael Metz <6386318+m-metz@users.noreply.github.com>
2023-03-15 22:16:47 -0600

    Remove unneeded template files and create a stock page structure

--- FrontEnd/src/views/HomeView.vue
+++ FrontEnd/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div class="home">
+      <h1>This is the home page</h1>
+    </div>
   </main>
 </template>
