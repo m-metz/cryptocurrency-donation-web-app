7d9e752b710afa69bcda0c589fa40aa3b5c979fc
Michael Metz <6386318+m-metz@users.noreply.github.com>
2023-03-16 02:52:48 -0600

    Turn router into MDBNavbar

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <header>
-    <nav>
-      <RouterLink :to="{ name: 'non-profits' }">Non-Profits</RouterLink>
-      |
-      <RouterLink :to="{ name: 'cryptocurrency-donations' }">
-        Cryptocurrency Donations History
+    <MDBNavbar expand="lg" light bg="light" container>
+      <RouterLink class="navbar-brand" :to="{ name: 'non-profits' }">
+        Benevity Cryptocurrency Donation App
       </RouterLink>
-    </nav>
+      <MDBNavbarToggler target="#navbarNav"></MDBNavbarToggler>
+      <MDBNavbarNav collapse="navbarNav">
+        <MDBNavbarItem :to="{ name: 'non-profits' }">Non-Profits</MDBNavbarItem>
+        <MDBNavbarItem :to="{ name: 'cryptocurrency-donations' }">
+          Cryptocurrency Donations History
+        </MDBNavbarItem>
+      </MDBNavbarNav>
+    </MDBNavbar>
   </header>
 
   <main>
@@ -15,8 +20,25 @@
 </template>
 
 <script>
+import { MDBNavbar, MDBNavbarToggler, MDBNavbarNav, MDBNavbarItem } from 'mdb-vue-ui-kit'
+
+import { ref } from 'vue'
+
 export default {
-  name: 'App'
+  components: {
+    MDBNavbar,
+    MDBNavbarToggler,
+    MDBNavbarNav,
+    MDBNavbarItem
+  },
+  setup() {
+    const collapse1 = ref(false)
+    const dropdown1 = ref(false)
+    return {
+      collapse1,
+      dropdown1
+    }
+  }
 }
 </script>
 
