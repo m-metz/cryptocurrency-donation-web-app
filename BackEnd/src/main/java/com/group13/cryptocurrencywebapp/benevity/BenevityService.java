39972ce9b72c0b57994723f0eaadc67a83830ec3
Felipe Gebara <feagmoreira@hotmail.com>
2023-03-23 10:39:31 -0600

    Get Donation Service

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/benevity/BenevityService.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/benevity/BenevityService.java
@@ -1,5 +1,28 @@
 package com.group13.cryptocurrencywebapp.benevity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
 public class BenevityService {
+
+    @Autowired
+    private WebClient webclient = WebClient.create();
+
+    public String getDonationStatus(String id) {
+        ResponseEntity<BenevityDonation> response = webclient.get()
+                .uri("/donations/" + id)
+                .retrieve()
+                .toEntity(BenevityDonation.class)
+                .block();
+
+        BenevityDonation donation = response.getBody();
+        String state = BenevityDonation.getState();
+
+        return state;
+
+    }
 
 }
