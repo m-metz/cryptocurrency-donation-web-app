771f8ad9ff993ee7a6ea60750961c73b222de868
Felipe Gebara <feagmoreira@hotmail.com>
2023-03-23 18:20:20 -0600

    Exchange Binance Test

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/controller/CryptoCurrencyDonationController.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/controller/CryptoCurrencyDonationController.java
@@ -9,20 +9,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.group13.cryptocurrencywebapp.entity.CryptoCurrencyDonation;
 import com.group13.cryptocurrencywebapp.service.CryptoCurrencyDonationService;
+import com.group13.cryptocurrencywebapp.service.ExchangeService;
+import com.group13.cryptocurrencywebapp.web_entity.exchange.ExchangeAccount;
 
 @RestController
 @RequestMapping(path = "api/v1/cryptocurrencydonation")
 public class CryptoCurrencyDonationController {
     private final CryptoCurrencyDonationService cryptoCurrencyDonationService;
+    private final ExchangeService exchangeService;
 
     @Autowired
-    public CryptoCurrencyDonationController(CryptoCurrencyDonationService cryptoCurrencyDonationService) {
+    public CryptoCurrencyDonationController(CryptoCurrencyDonationService cryptoCurrencyDonationService,
+            ExchangeService exchangeService) {
         this.cryptoCurrencyDonationService = cryptoCurrencyDonationService;
+        this.exchangeService = exchangeService;
     }
 
     @GetMapping
     public List<CryptoCurrencyDonation> getAllCryptoCurrencyDonations() {
         return cryptoCurrencyDonationService.getAllCryptoDonations();
+    }
+
+    @GetMapping(path = "exchange/account")
+    public ExchangeAccount getAccountInfo() throws Exception {
+        return exchangeService.getAccountInfo();
     }
 
 }
