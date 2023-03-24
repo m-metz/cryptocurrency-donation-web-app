aec47eb899cf8d15c027d1fd550e83c9fb58c005
AlexanderKruger <104882141+AlexanderKruger@users.noreply.github.com>
2023-03-24 11:16:47 -0600

    Adding API calls for BenevityDonation Adding API call to create donation Adding API call to get the status of a Benevity donation

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/controller/CryptoCurrencyDonationController.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/controller/CryptoCurrencyDonationController.java
@@ -4,25 +4,45 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.group13.cryptocurrencywebapp.entity.CryptoCurrencyDonation;
+import com.group13.cryptocurrencywebapp.service.BenevityService;
 import com.group13.cryptocurrencywebapp.service.CryptoCurrencyDonationService;
+import com.group13.cryptocurrencywebapp.web_entity.benevity.BenevityDonation;
 
 @RestController
 @RequestMapping(path = "api/v1/cryptocurrencydonation")
 public class CryptoCurrencyDonationController {
     private final CryptoCurrencyDonationService cryptoCurrencyDonationService;
+    private final BenevityService benevityService;
 
     @Autowired
-    public CryptoCurrencyDonationController(CryptoCurrencyDonationService cryptoCurrencyDonationService) {
+    public CryptoCurrencyDonationController(CryptoCurrencyDonationService cryptoCurrencyDonationService,
+                                            BenevityService benevityService) {
         this.cryptoCurrencyDonationService = cryptoCurrencyDonationService;
+        this.benevityService = benevityService;
     }
 
-    @GetMapping
+    @GetMapping()
     public List<CryptoCurrencyDonation> getAllCryptoCurrencyDonations() {
         return cryptoCurrencyDonationService.getAllCryptoDonations();
     }
+
+    @GetMapping("/Benevity/id={id}")
+    public String getBenevityDonation(@PathVariable String id){
+        return benevityService.getDonationStatus(id);
+    }
+
+    @PostMapping("/Benevity/createDonation")
+    public BenevityDonation createBenevityDonation(@RequestBody String benevityDonationStr){
+        
+        return benevityService.createDonation(benevityDonationStr);
+    }
+    
 
 }
--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/service/BenevityService.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/service/BenevityService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import com.group13.cryptocurrencywebapp.web_entity.benevity.BenevityDonation;
+import reactor.core.publisher.Mono;
 
 @Service
 public class BenevityService {
@@ -13,6 +15,20 @@ public class BenevityService {
     @Qualifier("benevityClient")
     private WebClient webclient = WebClient.create();
 
+    //Create a new donation
+    public BenevityDonation createDonation(String benevityDonationStr){
+
+        BenevityDonation response = webclient.post()
+
+                                            .uri("https://api.benevity-staging.org/donations")
+                                            .body(Mono.just(benevityDonationStr), String.class)
+                                            .retrieve()
+                                            .bodyToMono(BenevityDonation.class)
+                                            .block();
+        return response;
+    }
+
+    //Get the status of an existing donation
     public String getDonationStatus(String id) {
         ResponseEntity<BenevityDonation> response = webclient.get()
                 .uri("/donations/" + id)
@@ -21,10 +37,12 @@ public class BenevityService {
                 .block();
 
         BenevityDonation donation = response.getBody();
-        String state = BenevityDonation.getState();
+        String state = null;
 
+        if(!(donation == null)){
+            state = donation.retrieveStatus();
+        }
         return state;
-
     }
 
 }
