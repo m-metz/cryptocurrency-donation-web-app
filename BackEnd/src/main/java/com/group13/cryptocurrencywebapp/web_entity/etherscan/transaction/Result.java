855fbd294facb8851f70ac02bece6e27f472bb90
Felipe Gebara <feagmoreira@hotmail.com>
2023-03-29 14:24:03 -0600

    including transaction value

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/web_entity/etherscan/transaction/Result.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/web_entity/etherscan/transaction/Result.java
@@ -6,16 +6,18 @@ public class Result {
     String hash;
     String from;
     String gasUsed;
+    String value;
 
     public Result() {
 
     }
 
-    public Result(String timeStamp, String hash, String from, String gasUsed) {
+    public Result(String timeStamp, String hash, String from, String gasUsed, String value) {
         this.timeStamp = timeStamp;
         this.hash = hash;
         this.from = from;
         this.gasUsed = gasUsed;
+        this.value = value;
     }
 
     public String getTimeStamp() {
@@ -48,6 +50,14 @@ public class Result {
 
     public void setGasUsed(String gasUsed) {
         this.gasUsed = gasUsed;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 
 }
