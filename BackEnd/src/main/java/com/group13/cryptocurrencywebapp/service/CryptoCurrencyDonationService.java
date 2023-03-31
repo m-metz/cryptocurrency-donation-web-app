855d1dd5a317a08a1295ebf4d6d04a7b23ccfe67
Felipe Gebara <feagmoreira@hotmail.com>
2023-03-31 00:38:32 -0600

    Basic trade creation function + bug fixes

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/service/CryptoCurrencyDonationService.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/service/CryptoCurrencyDonationService.java
@@ -1,10 +1,12 @@
 package com.group13.cryptocurrencywebapp.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.stereotype.Service;
 import com.group13.cryptocurrencywebapp.entity.CryptoCurrencyDonation;
 import com.group13.cryptocurrencywebapp.entity.CryptoTransfer;
@@ -14,7 +16,8 @@ import com.group13.cryptocurrencywebapp.repository.CryptoCurrencyDonationReposit
 import com.group13.cryptocurrencywebapp.repository.CryptoTransferRepository;
 import com.group13.cryptocurrencywebapp.repository.FeeRepository;
 import com.group13.cryptocurrencywebapp.web_entity.etherscan.transaction.Result;
-import com.group13.cryptocurrencywebapp.repository.FeeRepository;
+import com.group13.cryptocurrencywebapp.web_entity.exchange.binance.ExchangeTradeResponse;
+import com.group13.cryptocurrencywebapp.web_entity.exchange.binance.Fill;
 import com.group13.cryptocurrencywebapp.repository.TradeRepository;
 
 @Service
@@ -25,19 +28,21 @@ public class CryptoCurrencyDonationService {
     private final EtherscanService etherscanService;
     private final TradeService tradeService;
     private final TradeRepository tradeRepository;
+    private final ExchangeService exchangeService;
 
     @Autowired
     public CryptoCurrencyDonationService(CryptoCurrencyDonationRepository cryptoCurrencyDonationRepository,
             CryptoTransferRepository cryptoTransferRepository,
             FeeRepository feeRepository,
             EtherscanService etherscanService,
-            TradeService tradeService, TradeRepository tradeRepository) {
+            TradeService tradeService, TradeRepository tradeRepository, ExchangeService exchangeService) {
         this.cryptoCurrencyDonationRepository = cryptoCurrencyDonationRepository;
         this.tradeService = tradeService;
         this.tradeRepository = tradeRepository;
         this.cryptoTransferRepository = cryptoTransferRepository;
         this.etherscanService = etherscanService;
         this.feeRepository = feeRepository;
+        this.exchangeService = exchangeService;
     }
 
     public CryptoCurrencyDonation createNewDonation(CryptoCurrencyDonation cryptoDonation) {
@@ -51,7 +56,7 @@ public class CryptoCurrencyDonationService {
         return cryptoCurrencyDonationRepository.findAll();
     }
 
-    public void createTrade(int donationId, float amount) {
+    public Trade createTrade(int donationId, float amount) throws InterruptedException {
         CryptoCurrencyDonation donation = cryptoCurrencyDonationRepository.findById(donationId).get();
         Trade newTrade = new Trade();
 
@@ -59,6 +64,37 @@ public class CryptoCurrencyDonationService {
         newTrade.setAmount(amount);
         newTrade.setTime(LocalDateTime.now());
         newTrade.setToCurrency("USD");
+
+        ExchangeTradeResponse response = exchangeService.executeNewTrade(amount);
+
+        if (response != null) {
+            newTrade.setExchangeReferenceId(response.getClientOrderId());
+            float convertedAmount = 0;
+            float comission = 0;
+            for (Fill fill : response.getFills()) {
+                convertedAmount = convertedAmount + Float.parseFloat(fill.getQty());
+                comission = comission + Float.parseFloat(fill.getCommission());
+            }
+            newTrade.setConvertedAmount(convertedAmount);
+            newTrade = tradeRepository.save(newTrade);
+
+            Fee comissionFee = new Fee(comission, newTrade, "Comission");
+            comissionFee = feeRepository.save(comissionFee);
+
+            List<Fee> fees = new ArrayList<>();
+            fees.add(comissionFee);
+
+            newTrade.setFees(fees);
+            newTrade = tradeRepository.save(newTrade);
+
+            donation.setTrade(newTrade);
+            donation = cryptoCurrencyDonationRepository.save(donation);
+
+            return newTrade;
+
+        } else {
+            return null;
+        }
 
     }
 
@@ -86,15 +122,24 @@ public class CryptoCurrencyDonationService {
         Fee gasFee = new Fee(Float.parseFloat(latest.getGasUsed()), deposit, "Gas");
         gasFee = feeRepository.save(gasFee);
 
-        deposit.setFees(Arrays.asList(new Fee[] { gasFee }));
+        List<Fee> fees = new ArrayList<>();
+        fees.add(gasFee);
+        // had to change because of immutable list
+        // deposit.setFees(Arrays.asList(new Fee[] { gasFee }));
+        deposit.setFees(fees);
         deposit.setFinal_amount(
                 deposit.getAmount() - Float.parseFloat(latest.getGasUsed()) / (float) 1000000000000000000.0);
 
         // deposit = cryptoTransferRepository.save(deposit);
         CryptoTransfer deposit1 = cryptoTransferRepository.findById(deposit.getTransactionId()).get();
 
+        donation.setCryptoTransfer(deposit1);
+        donation = cryptoCurrencyDonationRepository.save(donation);
+
         // gasFee.setTransaction(deposit1);
         // gasFee = feeRepository.save(deposit.getFees().get(0));
+
+        // INSERT CONNECTION TO TRADE HERE
 
         return deposit1;
     }
