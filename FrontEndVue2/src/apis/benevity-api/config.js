228d29c6eac4d04c57e0ec657c85989ea597df3b
Michael Metz <6386318+m-metz@users.noreply.github.com>
2023-04-06 03:25:32 -0600

    Allow configs now because they won't have sensitive info.

--- FrontEndVue2/src/apis/benevity-api/config.js
+++ FrontEndVue2/src/apis/benevity-api/config.js
@@ -9,4 +9,14 @@ export default {
   The url to connect to our Benevity API Proxy.
   */
   baseUrl: "http://localhost:8080",
+
+  /*
+  Required.
+
+  If true, use mock JSON data (if available) if the API has not been
+  implemented yet.
+
+  If false, raise an error if the API is not implemented.
+  */
+  useMockForUnimplemented: true,
 };
