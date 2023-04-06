8c3ee520762afc8b89c78ed6069c6bf2942709b0
Michael Metz <6386318+m-metz@users.noreply.github.com>
2023-04-06 00:07:47 -0600

    Remove old Benevity API config

--- FrontEndVue2/src/apis/benevity-api/benevity-api.js
+++ FrontEndVue2/src/apis/benevity-api/benevity-api.js
@@ -1,5 +1,3 @@
-import { Buffer } from "buffer";
-
 import "isomorphic-fetch";
 
 import { isEmptyObject } from "@/helpers/index.js";
@@ -15,18 +13,7 @@ export default class BenevityApi {
 
   // API authentication configuration
   #baseUrl;
-  #clientId;
-  #clientSecret;
-  #grantType;
-  #scopes;
-  #adapter;
-  #companyId;
-  #apiKey;
-  #viteProxyUrl;
-
-  // OAuth token tracking
-  #tokenExpiryTime;
-  #accessToken;
+  // #useMocks;
 
   constructor(config) {
     if (BenevityApi.#instance) {
@@ -37,111 +24,20 @@ export default class BenevityApi {
     BenevityApi.#instance = this;
 
     if (isEmptyObject(config)) {
-      throw new Error("BenevityApi config is not defined or an empty object.");
+      throw new Error(
+        "BenevityApi config is not defined or is an empty object."
+      );
     }
 
     BenevityApi.#instance.#initialize(config);
   }
 
   /*
-  This actually is used, just not on the this object. VSCode ts syntax highlighting can't detect that it's used.
+  This actually is used, just not on the `this` object. VSCode ts syntax highlighting can't detect that it's used.
   */
   #initialize(config) {
     if (!isEmptyObject(config)) {
-      Object.assign(this, config);
-      if (typeof this.#viteProxyUrl === "string") {
-        this.baseUrl = this.#viteProxyUrl;
-      }
+      this.#baseUrl = config.baseUrl;
     }
-  }
-
-  refreshApiAuthentication() {
-    // Set variables
-    var currentTime = Date.now();
-
-    // Get a new token if current one expired, if no token exists, or if no expiryTime exists
-    if (
-      currentTime >= this.#tokenExpiryTime ||
-      !this.#accessToken ||
-      !this.#tokenExpiryTime
-    ) {
-      let tokenUrl = this.#baseUrl + "/oauth2/token";
-
-      let options = {
-        method: "POST",
-        // credentials: "include",
-        headers: {
-          authorization:
-            "Basic " +
-            Buffer.from(`${this.#clientId}:${this.#clientSecret}`).toString(
-              "base64"
-            ),
-          "content-type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-          grant_type: this.#grantType,
-          scope: this.#scopes,
-        }),
-      };
-      fetch(tokenUrl, options)
-        .then(function (response) {
-          if (!response.ok) {
-            console.log(response.text());
-            throw new Error("Could not get OAuth token");
-          }
-          return response.json();
-        })
-        .then(function (json) {
-          console.log("parsed json", json);
-        })
-        .catch(function (ex) {
-          console.error(ex);
-        });
-      // pm.sendRequest(options, function (err, response) {
-      //   this.#accessToken = response.json().access_token;
-
-      //   // Set the ExpiresInTime variable to the time given in the response if it exists
-      //   if (response.json().expires_in) {
-      //     this.#tokenExpiryTime =
-      //       currentTime + response.json().expires_in * 0.9 * 1000;
-      //   }
-      // });
-    }
-  }
-
-  get baseUrl() {
-    return this.baseUrl;
-  }
-
-  set baseUrl(value) {
-    this.#baseUrl = value;
-  }
-
-  set clientId(value) {
-    this.#clientId = value;
-  }
-
-  set clientSecret(value) {
-    this.#clientSecret = value;
-  }
-
-  set grantType(value) {
-    this.#grantType = value;
-  }
-
-  set adapter(value) {
-    this.#adapter = value;
-  }
-
-  set companyId(value) {
-    this.#companyId = value;
-  }
-
-  set apiKey(value) {
-    this.#apiKey = value;
-  }
-
-  set viteProxyUrl(value) {
-    this.#viteProxyUrl = value;
   }
 }
