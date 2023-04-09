fdf101d28ff7aa4877b2217bfaf9f5535f9da37a
Felipe Gebara <feagmoreira@hotmail.com>
2023-04-09 17:18:16 -0600

    oath reconfig for accepting scheduled methods also

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/Config/OAuth2ClientManagerConfig.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/Config/OAuth2ClientManagerConfig.java
@@ -2,9 +2,11 @@ package com.group13.cryptocurrencywebapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
@@ -15,7 +17,7 @@ public class OAuth2ClientManagerConfig {
         @Bean
         public OAuth2AuthorizedClientManager clientManager( //
                         ClientRegistrationRepository clientRegistrationRepository, //
-                        OAuth2AuthorizedClientRepository authorizedClientRepository) {
+                        OAuth2AuthorizedClientService clientService) {
 
                 OAuth2AuthorizedClientProvider clientProvider = OAuth2AuthorizedClientProviderBuilder.builder() //
                                 .authorizationCode() //
@@ -23,8 +25,8 @@ public class OAuth2ClientManagerConfig {
                                 .clientCredentials() //
                                 .build();
 
-                DefaultOAuth2AuthorizedClientManager clientManager = new DefaultOAuth2AuthorizedClientManager(
-                                clientRegistrationRepository, authorizedClientRepository);
+                AuthorizedClientServiceOAuth2AuthorizedClientManager clientManager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+                                clientRegistrationRepository, clientService);
                 clientManager.setAuthorizedClientProvider(clientProvider);
 
                 return clientManager;
