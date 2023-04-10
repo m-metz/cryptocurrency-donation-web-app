b6303fc4276626e877e10e00fd3d607aa9aaa2a2
Felipe Gebara <feagmoreira@hotmail.com>
2023-04-09 19:24:41 -0600

    javadocs apis

--- BackEnd/src/main/java/com/group13/cryptocurrencywebapp/api/EtherscanApi.java
+++ BackEnd/src/main/java/com/group13/cryptocurrencywebapp/api/EtherscanApi.java
@@ -9,11 +9,38 @@ import io.netty.handler.logging.LogLevel;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
+/**
+ * <pre>
+ * Class Name: EtherscanApi
+ * 
+ * Date Created: March 15, 2023
+ * Company: Benevity
+ * </pre>
+ * 
+ * <p>
+ * Class that configures model Bean definition for WebClient object to be used
+ * to make https requests to Etherscan API endpoints.
+ * </p>
+ * 
+ * @author U of C ENSF609 Capstone 2023 (Alex K, Felipe G, Mike, M)
+ * 
+ * @Since April 09, 2023
+ * 
+ */
 @Configuration
 public class EtherscanApi {
 
+    /**
+     * API_ENDPOINT basic URL for Etherscan Goerli Network
+     */
     static String API_ENDPOINT = "https://api-goerli.etherscan.io/api";
 
+    /**
+     * Bean model that builds etherscanClient WebClient object to be used for the
+     * http requests to Etherscan including base URL and logging.
+     * 
+     * @return WebClient object that will be injected automatically by Spring Boot.
+     */
     @Bean
     WebClient etherscanClient() {
         return WebClient.builder()
