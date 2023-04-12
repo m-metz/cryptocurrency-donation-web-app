2ce4163806510e210925c08b1ad3eca1c2e46911
Michael Metz <6386318+m-metz@users.noreply.github.com>
2023-04-12 05:11:12 -0600

    Create Web App API

---
+++ FrontEndVue2/src/apis/cryptocurrency-donation-web-app-api/config.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} config
+ * @property {string} baseUrl The url to connect to our Cryptocurrency Donation Web App Api.
+ * @property {string} toCryptoAddress The crypto address to send the donations to.
+ */
+
+export default {
+  baseUrl: "http://localhost:8080/api/v1",
+  toCryptoAddress: null,
+};
